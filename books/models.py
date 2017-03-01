091e4c247cb640fdf3978015a017e1be2ec78d37
NerdBuff <thompson.jason.f@gmail.com>
2017-03-01 10:20:18 -0500

    Added first URLs for home page and simple HTTP response in view

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -17,7 +18,7 @@ class Book(models.Model):
     def list_authors(self):
         return ", ".join([author.name for author in self.authors.all])
 
-    def save(self, *args. **kwargs):
+    def save(self, *args, **kwargs):
         if (self.review and self.date_reviewed is None):
             self.date_reviewed=now()
 
--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def list_books(request):
+    return HttpResponse(request.user.username)
+    #return HttpResponse("We can put anything here") #This puts a simple HTML text in the page
