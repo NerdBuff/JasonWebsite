88d7d1bfe71f7abc105bf1932d2dc2e65c5a0286
NerdBuff <thompson.jason.f@gmail.com>
2017-02-28 22:09:01 -0500

    Created Relationship Between Models

--- books/models.py
+++ books/models.py
@@ -4,11 +4,16 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70, help_text="Use pen name, not real name.")
+    author = models.ManyToManyField("Author", related_name="Books")
     review = models.TextField(blank=True, null=True)
     date_reviewed = models.DateTimeField(blank=True, null=True)
     is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
-    
+
     def __str__(self):
         return self.title
-  
+
+class Author(models.Model):
+    name = models.CharField(max_length=70 , help_text="Use pen name, not real name.")
+
+    def __str__(self):
+        return self.name
